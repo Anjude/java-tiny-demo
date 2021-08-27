402d4a6e2eba536c01b62365f20513cb4b8f63de
wanghairui <aboy007262@163.com>
2021-08-27 17:30:28 +0800

    0827,配置文件

--- demo-api-encrypt/src/main/java/xyz/anjude/encrypt/EncryptApplication.java
+++ demo-api-encrypt/src/main/java/xyz/anjude/encrypt/EncryptApplication.java
@@ -1,10 +1,8 @@
 package xyz.anjude.encrypt;
 
 import org.mybatis.spring.annotation.MapperScan;
-import org.mybatis.spring.annotation.MapperScans;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
 /**
  * EncryptApplication  TODO
